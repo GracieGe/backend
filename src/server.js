5fca3eb964368c3a510b7043727153c841c797e6
GracieGe <ruiyi.ge2001@gmail.com>
2024-07-14 20:07:30 +0100

    add pictures

--- src/server.js
+++ src/server.js
@@ -10,6 +10,9 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', routes);
 
+// 配置静态文件目录
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
