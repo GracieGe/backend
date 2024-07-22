75913315b207a65d769b7b6b107ad58562a4f88b
GracieGe <ruiyi.ge2001@gmail.com>
2024-07-22 17:13:49 +0100

    images to courses

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./src/routes');
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 app.use(bodyParser.json());
